f62b9c51b1350e7c68033334518573922fb8b1ec
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-09-22 20:46:47 -0400

    add: input lable

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -2,12 +2,25 @@ import { FC, InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  labelClassName?: string;
   className?: string;
 }
 
-export const Input: FC<InputProps> = ({ className, label, ...props }) => (
-  <input
-    {...props}
-    className={`${className} font-nunito font-normal text-base px-4 py-2 rounded-lg bg-dark-08 text-dark focus:outline-brand focus:outline-2`}
-  />
+export const Input: FC<InputProps> = ({
+  className,
+  label,
+  labelClassName,
+  ...props
+}) => (
+  <div className="flex flex-col gap-2 font-nunito">
+    {label && (
+      <label className={`${labelClassName} text-dark font-bold text-[18px]`}>
+        {label}
+      </label>
+    )}
+    <input
+      {...props}
+      className={`${className} font-normal text-base px-4 py-2 rounded-lg bg-dark-08 text-dark focus:outline-brand focus:outline-2`}
+    />
+  </div>
 );
