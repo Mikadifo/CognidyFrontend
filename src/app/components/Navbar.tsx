6d31cfddf95e1f7546f0c6c15202df72c4b155b9
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-09-22 21:16:18 -0400

    add: navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import Logo from "./../assets/logoHorizontal.svg";
+import { Button } from "./Button";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  return (
+    <div
+      className={`flex w-full border-b border-dark-16 px-8 py-2 justify-between sticky top-0 ${className}`}
+    >
+      <Logo height={48} />
+      <Button className="!bg-brand" as="a" href="/dashboard">
+        Dashboard
+      </Button>
+    </div>
+  );
+};
