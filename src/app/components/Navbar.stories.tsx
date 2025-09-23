b0ca06e64f955d0896c000c70a43474300cdbdad
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-09-23 02:29:14 -0400

    add: navbar docs

--- src/app/components/Navbar.stories.tsx
+++ src/app/components/Navbar.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Navbar } from "./Navbar";
 
 const meta: Meta<typeof Navbar> = {
-  title: "Navbar",
+  title: "Landing Navigation",
   component: Navbar,
   parameters: {
     docs: {
@@ -19,6 +19,6 @@ export default meta;
 
 type Story = StoryObj<typeof Navbar>;
 
-export const Default: Story = {
+export const NavBar: Story = {
   args: {},
 };
---
+++ src/app/components/SideBar.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SideBar } from "./SideBar";
+
+const meta: Meta<typeof SideBar> = {
+  title: "Dashboard Navigation",
+  component: SideBar,
+  parameters: {
+    docs: {
+      description: {
+        component: "This sidebar is used for the dashboard page routes only",
+      },
+    },
+  },
+  argTypes: {},
+  args: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SideBar>;
+
+export const Sidebar: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: "/dashboard/study",
+      },
+    },
+  },
+  args: {},
+};
