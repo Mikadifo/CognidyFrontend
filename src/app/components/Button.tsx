01819ca65acce2fe9d86ca14b8b9996375c48798
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-09-22 21:16:07 -0400

    add: link support

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,31 +1,69 @@
-import { ComponentType, FC, SVGProps } from "react";
+import Link from "next/link";
+import {
+  AnchorHTMLAttributes,
+  ButtonHTMLAttributes,
+  ComponentType,
+  FC,
+  SVGProps,
+} from "react";
 
-interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
-  type?: "button" | "submit" | "reset";
+interface BaseButtonProps {
   className?: string;
-  disabled?: boolean;
   icon?: ComponentType<SVGProps<SVGSVGElement>>;
   variant?: "filled" | "outline";
+  href?: string;
 }
 
+interface asButtonProps
+  extends BaseButtonProps,
+    ButtonHTMLAttributes<HTMLButtonElement> {
+  as?: "button";
+  href?: never;
+}
+
+interface asLinkProps
+  extends BaseButtonProps,
+    AnchorHTMLAttributes<HTMLAnchorElement> {
+  as?: "a";
+  href: string;
+}
+
+type ButtonProps = asButtonProps | asLinkProps;
+
 export const Button: FC<ButtonProps> = ({
-  children,
-  onClick,
-  type = "button",
+  as = "button",
   className,
-  disabled,
   icon: Icon,
   variant = "filled",
-}) => (
-  <button
-    type={type}
-    disabled={disabled}
-    onClick={onClick}
-    className={`${className} font-poppins font-normal text-base px-6 py-3 rounded-sm ${variant === "filled" ? "bg-dark text-white hover:opacity-80" : "border border-dark text-dark hover:bg-dark-08"} cursor-pointer ${Icon && "flex gap-2 items-center"}`}
-  >
-    {Icon && <Icon className="size-5" />}
-    {children}
-  </button>
-);
+  href,
+  ...props
+}) => {
+  const styles = `${className} font-poppins font-normal text-base px-6 py-3 rounded-sm ${variant === "filled" ? "bg-dark text-white hover:opacity-80" : "border border-dark text-dark hover:bg-dark-08"} cursor-pointer ${Icon && "flex gap-2 items-center"}`;
+  const children = (
+    <>
+      {Icon && <Icon className="size-5" />}
+      {props.children}
+    </>
+  );
+
+  if (as === "a" && href) {
+    return (
+      <Link
+        {...(props as AnchorHTMLAttributes<HTMLAnchorElement>)}
+        href={href}
+        className={styles}
+      >
+        {children}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        {...(props as ButtonHTMLAttributes<HTMLButtonElement>)}
+        className={styles}
+      >
+        {children}
+      </button>
+    );
+  }
+};
