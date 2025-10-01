4eee8be1909698c35604a78fe336b0861b80f6ed
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-10-01 00:08:01 -0400

    add: Notes section

---
+++ src/app/components/NoteItems.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { Dispatch, FC, SetStateAction } from "react";
+import { IconButton } from "@/app/components/IconButton";
+import { Note } from "../models/Note";
+import DeleteIcon from "@/app/assets/icons/trashcan.svg";
+import PdfIcon from "@/app/assets/icons/pdf.svg";
+import MdIcon from "@/app/assets/icons/markdown.svg";
+import TxtIcon from "@/app/assets/icons/textNotes.svg";
+
+interface NoteItemsProps {
+  notes: Note[];
+  setNotes: Dispatch<SetStateAction<Note[]>>;
+}
+
+function getIconFromSource(source: string) {
+  const extension = source.split(".").pop();
+
+  switch (extension) {
+    case "pdf":
+      return <PdfIcon className="size-4" />;
+    case "md":
+      return <MdIcon className="size-4" />;
+    case "txt":
+      return <TxtIcon className="size-4" />;
+
+    default:
+      // TODO: change this to a default undefined icon
+      <span>X</span>;
+  }
+}
+
+export const NoteItems: FC<NoteItemsProps> = ({ notes, setNotes }) => {
+  const handleDeleteSource = (id: number) => {
+    const confirmed = confirm(
+      "All flashcards, puzzles and roadmaps generated using this file will also be deleted. Are you sure you want to continue?",
+    );
+
+    if (confirmed) {
+      //TODO: call DB to remove the note with the id
+      setNotes((prev) => prev.filter((source) => source.id !== id));
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      {notes.length > 0 ? (
+        notes.map(({ id, source }) => (
+          <div className="flex gap-2" key={id}>
+            <div className="flex gap-2 bg-dark-08 rounded-lg px-4 py-2 w-full items-center">
+              {getIconFromSource(source)}
+              {source}
+            </div>
+            <IconButton
+              icon={DeleteIcon}
+              onClick={() => handleDeleteSource(id)}
+            />
+          </div>
+        ))
+      ) : (
+        <span>
+          You don't have any notes uploaded yet.{" "}
+          <b>Chose a file and press Upload new note.</b>
+        </span>
+      )}
+    </div>
+  );
+};
