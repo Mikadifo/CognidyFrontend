0c3af8a47202cb7d97bfb5ea53f61803502d8285
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-10-03 18:40:41 -0400

    add: CTA and footer

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/landing/Footer";
 import Hero from "../components/landing/Hero";
 import HowItWorks from "../components/landing/HowItWorks";
 import MVPs from "../components/landing/MVPs";
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <HowItWorks />
       <MVPs />
+      <Footer />
     </div>
   );
 }
