4612831a64389cd6c2cab3637fa4fa0c1b526dba
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-10-03 17:59:52 -0400

    add: MVPs section

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/landing/Hero";
 import HowItWorks from "../components/landing/HowItWorks";
+import MVPs from "../components/landing/MVPs";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <HowItWorks />
+      <MVPs />
     </div>
   );
 }
