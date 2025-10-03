7778cafce2e64aca701155b4bea83bf1d633a617
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-10-03 17:39:04 -0400

    fix: navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,7 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({ className }) => {
   return (
     <header
-      className={`flex w-full bg-white border-b border-dark-16 px-8 py-2 justify-between sticky top-0 ${className}`}
+      className={`flex w-full bg-white border-b border-dark-16 px-8 py-2 justify-between sticky top-0 z-10 ${className}`}
     >
       <Logo height={48} />
       <Button className="!bg-brand" as="a" href="/dashboard">
