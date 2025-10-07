802f02624a1b5bd5b406a1b12a4a19e0f3ec5620
Ludwin Tenezaca <ludwin.tenezaca38@myhunter.cuny.edu>
2025-10-07 14:09:45 -0400

    added beginning UI of the learning section, button to take user to Puzzle section

--- src/app/dashboard/learning/page.tsx
+++ src/app/dashboard/learning/page.tsx
@@ -1,8 +1,18 @@
+import { PuzzlesButton } from '@/app/components/PuzzlesButton';
+
 export const metadata = {
   title: "Cognidy | Learning",
   description: "Learning section",
 };
 
 export default function Learning() {
-  return <div>Learning</div>;
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="font-poppins text-3xl font-bold mb-8">Learning</h1>
+      <PuzzlesButton />
+      <h2 className="font-poppins text-3xl font-bold mt-8">
+        Coming Soon!
+      </h2>
+    </div>
+  );
 }
