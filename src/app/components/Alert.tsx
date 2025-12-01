91471eab3d13183a6f10ac6f625cf9433a519a6c
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-12-01 17:56:38 -0500

    add: alert component

---
+++ src/app/components/Alert.tsx
@@ -0,0 +1,37 @@
+import CloseIcon from "../assets/icons/closeIcon.svg";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import AlertDto from "../dtos/AlertDto";
+
+interface AlertProps {
+  alert: AlertDto;
+  setAlert: Dispatch<SetStateAction<AlertDto>>;
+  closeAfter?: number | null;
+}
+
+function Alert({ alert, setAlert, closeAfter = null }: AlertProps) {
+  useEffect(() => {
+    if (alert.open) {
+      if (closeAfter) {
+        setTimeout(() => setAlert({ ...alert, open: false }), closeAfter);
+      }
+    }
+  }, [alert, setAlert]);
+
+  return (
+    <div
+      className={`${alert.severity === "error" ? "bg-red text-white" : "bg-green text-dark"} font-nunito px-6 py-3 rounded-lg whitespace-nowrap fixed bottom-6 left-1/2 -translate-x-1/2 flex gap-4 z-40 text-base`}
+      hidden={!alert.open}
+    >
+      <span>{alert.message}</span>
+      <button
+        type="button"
+        className="cursor-pointer"
+        onClick={() => setAlert((prev) => ({ ...prev, open: false }))}
+      >
+        <CloseIcon className="size-5" />
+      </button>
+    </div>
+  );
+}
+
+export default Alert;
