d078d91bb74b9f67f26982c0e0b934a634ef71f9
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-12-16 20:30:28 -0500

    mod: files

--- src/app/hooks/useAuth.ts
+++ src/app/hooks/useAuth.ts
@@ -1,27 +1,20 @@
-"use client";
-
 import { useRouter } from "next/navigation";
 
 export function useAuth() {
   const router = useRouter();
-  const isBrowser = typeof window !== "undefined";
-
 
   // Save token (for login or signup)
   const saveUser = (token: string) => {
-    if (!isBrowser) return;
     localStorage.setItem("token", token);
   };
 
   // Get token (used to check authentication)
   const getToken = (): string | null => {
-    if (!isBrowser) return null;
     return localStorage.getItem("token");
   };
 
   // Logout function (used in Sidebar)
   const logout = () => {
-    if (!isBrowser) return; 
     localStorage.removeItem("token");
     router.push("/login");
   };
